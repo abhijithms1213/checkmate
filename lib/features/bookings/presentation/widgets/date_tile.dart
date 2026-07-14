c622a1f458c6663221a1bcdf1e3d5e332ff7ed03
abhijithms1213 <abhijithms1213@gmail.com>
2026-07-14 06:53:29 +0530

    feat: Add DateTileWidget for displaying date information in bookings

---
+++ lib/features/bookings/presentation/widgets/date_tile.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class DateTileWidget extends StatelessWidget {
+  const DateTileWidget({
+    super.key,
+    required this.selected,
+    required this.dates, required this.index,
+  });
+  final bool selected;
+  final List<Map<String, String>> dates;
+  final int index;
+
+  static const Color darkBlue = Color(0xFF081E36);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 72,
+      decoration: BoxDecoration(
+        color: selected ?  darkBlue: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.grey.shade300),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            dates[index]["day"]!,
+            style: TextStyle(color: selected ? Colors.white : Colors.grey),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            dates[index]["date"]!,
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: selected ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
