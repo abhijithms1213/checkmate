addb3893465b7bff02cba96a20dca6672421c554
abhijithms1213 <abhijithms1213@gmail.com>
2026-07-14 06:54:00 +0530

    feat: Integrate ArticleBloc and update main app structure for article management

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,40 @@
 import 'package:checkmate/core/theme/app_theme.dart';
 import 'package:checkmate/features/auth/presentation/pages/login.dart';
 import 'package:checkmate/features/auth/presentation/pages/splash.dart';
+import 'package:checkmate/features/news/domain/usecases/get_article.dart';
+import 'package:checkmate/features/news/presentation/bloc/article/article_bloc.dart';
+import 'package:checkmate/features/news/presentation/bloc/article/article_event.dart';
+import 'package:checkmate/features/news/presentation/pages/article_page.dart';
+import 'package:checkmate/injection_container.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  initializeDependencies();
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
-    ),
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      home: const SplashScreen(),
+    return BlocProvider<ArticleBloc>(
+      // create: (context) => BlocProvider.of<ArticleBloc>(context)..add(GetArticles()),
+      create: (context) => s1()..add(GetArticles()),
+      child: MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        // home: const SplashScreen(),
+        home: ArticlePage(),
+      ),
     );
   }
 }
