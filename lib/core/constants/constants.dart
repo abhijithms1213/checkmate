22bbc31e52f3e8c112439c9f7ce210afbf3ae064
abhijithms1213 <abhijithms1213@gmail.com>
2026-07-14 08:50:23 +0530

    feat: Add Supabase URL and publishable key to constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsbaseApiUrl = "https://saurav.tech/NewsAPI/top-headlines";
+const String supabaseUrl = 'https://ysrkkgbnezarxzdyawyo.supabase.co';
+const String supabasePublishableKey =
+    'sb_publishable_pO5KkAoF6ztGGjbARXB4zw_GyvqN7o3';
