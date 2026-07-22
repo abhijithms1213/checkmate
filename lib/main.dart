88ada1962bd1882c84d9d2b769fe20631bd7b85e
abhijithms1213 <abhijithms1213@gmail.com>
2026-07-22 21:28:44 +0530

    feat: Remove unused imports and DevicePreview from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,8 @@ import 'package:checkmate/core/constants/constants.dart';
 import 'package:checkmate/core/theme/app_theme.dart';
 import 'package:checkmate/features/address/presentation/bloc/user_bloc.dart';
 import 'package:checkmate/features/auth/presentation/bloc/otp/otp_bloc.dart';
-import 'package:checkmate/features/auth/presentation/pages/login.dart';
 import 'package:checkmate/features/auth/presentation/pages/splash.dart';
-import 'package:checkmate/features/news/domain/usecases/get_article.dart';
-import 'package:checkmate/features/news/presentation/bloc/article/article_bloc.dart';
-import 'package:checkmate/features/news/presentation/bloc/article/article_event.dart';
-import 'package:checkmate/features/news/presentation/pages/article_page.dart';
 import 'package:checkmate/injection_container.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -27,10 +20,10 @@ Future<void> main() async {
   await initializeDependencies();
 
   runApp(
-    DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
+    MyApp(),
+    // DevicePreview(enabled: false, builder: (context) => const MyApp()),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
